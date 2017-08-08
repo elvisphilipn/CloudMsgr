acd2d6120f08448f7906bffa0482a6f78c671383
Elvis-Philip N <elvisphilipn@gmail.com>
2017-08-07 23:28:39 -0400

    Adding REST API for user management

---
+++ src/api/controllers/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function createUser(req, res) {
+  // res.status(404).send(`<h1>404: Invalid name: ${name}</h1>`);
+}
+
+function getUserInfo(req, res) {}
+
+function getParticipants(req, res) {}
+
+module.exports = {
+  createUser, createUser,
+  getUserInfo: getUserInfo,
+  getParticipants: getParticipants
+};
